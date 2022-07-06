4630b4c9709fb6a4746829084f186c90399a1656
F8716814 Rogerio Ghiraldeli Botelho de Paula <rogerio.ghiraldeli@bb.com.br>
2022-07-06 13:45:30 -0300

    melhorando o menu

--- BancoNovo/src/Aplicacao.java
+++ BancoNovo/src/Aplicacao.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class Aplicacao {
     public static void main(String[] args) throws Exception {
 		String[] opcoes1 = {"SAIR", "ABRIR CONTA", "SACAR", "DEPOSITAR", "TRANSFERÊNCIA", "INVESTIR", "CONSULTAR SALDO"};
-        Aplicacao menu = new Aplicacao();
         
 		Scanner sc = new Scanner(System.in);
 		int opcao = 100;
-        menu.print(opcoes1);
+        Aplicacao.print(opcoes1);
         while(opcao!= 0 ){
             opcao = sc.nextInt();
             System.out.printf("Você selecionou a opção: %s", opcoes1[opcao]);
@@ -19,13 +18,13 @@ public class Aplicacao {
             }
         }
         sc. close();
-		System.out.println("---------------- FIM -------------------");
+		System.out.println("\n-------------------- FIM ---------------------");
     }
     
-    public void print(String[] funcionalidades) {
+    public static void print(String[] funcionalidades) {
         System.out.println("-------------------- MENU --------------------");
         for (int i = 0; i < funcionalidades.length; i++) {
-            System.out.printf("%2d - %s\n", i, funcionalidades[i]);
+            System.out.printf("%02d - %s\n", i, funcionalidades[i]);
         }
         System.out.println("DIGITE A OPÇÃO: ");
     }
